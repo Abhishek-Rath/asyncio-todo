4fcad159b5412324f979b781ec189fc26e5b0416
Abhishek Rath <60350731+Abhishek-Rath@users.noreply.github.com>
2023-08-03 12:41:25 +0530

    Add update and delete functionality

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ from sqlalchemy.future import select
 
 from models import Todo, SessionFactory
 
-
+# Get todos
 async def get_todo(request):
     async with SessionFactory() as session:
         todos = await session.execute(select(Todo))
         todo_list = [{"id": todo.id, "title": todo.title, "completed": todo.completed} for todo in todos.scalars()]
-        print(todo_list)
+        # print(todo_list)
         return web.json_response(todo_list)
 
+# Post todo
 async def create_todo(request):
     data = await request.json()
     async with SessionFactory() as session:
@@ -24,10 +25,44 @@ async def create_todo(request):
             "id":todo.id,
         })
 
+# Update todo
+async def update_todo(request):
+    data = await request.json()
+    # print(request)
+    todo_id = int(request.match_info['todo_id'])
+
+    async with SessionFactory() as session:
+        todo = await session.get(Todo, todo_id)
+        if not todo:
+            return web.json_response({
+                'error': "Todo not found!"
+            })
+        
+        todo.title = data.get('title', todo.title)
+        todo.completed = data.get('completed', todo.completed)
+        await session.commit()
+        return web.json_response({
+            "message": "Todo updated successfully!"}, status=404)
+
+# Delete todo
+async def delete_todo(request):
+    todo_id = int(request.match_info['todo_id'])
+    async with SessionFactory() as session:
+        todo = await session.get(Todo, todo_id)
+        if not todo:
+            return web.json_response({"message": "Todo doesn't exist!"}, status=404)
+        await session.delete(todo)
+        await session.commit()
+        return web.json_response({
+            "message": "Todo deleted successfully!"
+        })
+
 async def main():
     app = web.Application()
     app.router.add_post('/todos', create_todo)
     app.router.add_get('/todos', get_todo)
+    app.router.add_put('/todos/{todo_id}', update_todo)
+    app.router.add_delete('/todos/{todo_id}', delete_todo)
     return app
 
 
